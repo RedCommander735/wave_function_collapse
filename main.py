5e92b33b63d12a27ca68a7d7a460b0df3f2c5690
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2023-01-21 23:56:21 +0100

    Added comments for better overview

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from PIL import Image
 # complete start from scratch
 
 class Tile:
-    def __init__(self, coords: tuple, neighbours: list, grid_size: int, ttype: str = "", collapsed = False):
+    def __init__(self, coords: tuple, possible_states: list,valid_neighbours: dict, grid_size: int, ttype: str = "", collapsed = False):
+        # TODO load the list of all possible states into every tile on creation
+        #
         self.ttype = ttype
         self.file = None
         self.coords = coords
         self.collapsed = collapsed
-        #self.valid_neighbours: dict = valid_neighbours
-        self.possible_neighbours: list = neighbours
+        self.valid_neighbours: dict = valid_neighbours
+        self.possible_states: list = possible_states
         self.grid_size: int = grid_size
         self.north = (max(self.coords[0] - 1, 0),               self.coords[1])
         self.east  = (self.coords[0],                           min(self.coords[1] + 1, grid_size - 1))
@@ -80,10 +82,12 @@ def main():
     elif len(sys.argv) > 1:
         file = sys.argv[1]
 
+    # Get all data and seed from file
     with open(f'json/{file}.json', 'r+', encoding="Utf-8") as openedfile:
         data = json.load(openedfile)
         seed = data["meta"]["seed"]
 
+        # If there is no seed specified use current tile as seed; NEEDS REWORK
         if len(seed) == 0:
             seed = str(time.time())
             data["meta"]["seed"] = seed
@@ -92,16 +96,21 @@ def main():
 
     random.seed(seed)
 
+    # Raise exception if not every specified type has its own image file specified
     if len(data["types"]) != len(data["image_files"]):
         raise Exception("Types and file names don't have equal count")
 
+    # Load the actual image files into storage instead of reference names
     for index, _file in enumerate(data["image_files"]):
         data["image_files"][index] = Image.open(f"src/{_file}")
 
+    # Grid size
     size = data["meta"]["size"]
     generate_image(size, data, file)
 
+
 def generate_image(size, valid_neighbours, _file):
+    # Define and generate initial grid
     grid = []
     for y_pos in range(size):
         row = []
@@ -110,13 +119,16 @@ def generate_image(size, valid_neighbours, _file):
             row.append(tile)
         grid.append(row)
 
+    # Update all tiles so the have their neighbouring tiles cached
     for y_pos in range(size):
         for x_pos in range(size):
             grid[y_pos][x_pos].neighbouring_tiles(grid)
 
+    # Define a random starting position for the algorithm
     x_pos = random.randint(0, size - 1)
     y_pos = random.randint(0, size - 1)
 
+    # Initialize first tile; EVERYTHIN BELOW NEEDS REWORK
     options = valid_neighbours["types"]
     index = random.randint(0, len(options) - 1)
     
@@ -130,12 +142,13 @@ def generate_image(size, valid_neighbours, _file):
     tile_size = valid_neighbours["meta"]["tile_size"]
 
     
-
+    # Generate clean canvas to later add tiles
     output = Image.new("RGB", (size*tile_size, size*tile_size))
 
-    # main loop
+    # Main loop
     total_start = time.perf_counter()
     while True:
+        # Iterate over the whole grid, update all tiles and check if finished; SHOULD WORK FINE
         start = time.perf_counter()
         finished = True
         for y_pos in range(size):
@@ -145,6 +158,7 @@ def generate_image(size, valid_neighbours, _file):
                     finished = False
         duration = time.perf_counter() - total_start
 
+        # Display time of iteration
         dur = f'{duration:.2f}s'
         if duration > 60:
             duration = duration / 60
@@ -156,6 +170,7 @@ def generate_image(size, valid_neighbours, _file):
             print("\n----- Finished -----")
             break
 
+    # Convert grid to Image; SHOULD WORK
     for y_pos in range(size):
         for x_pos in range(size):
             output.paste(grid[y_pos][x_pos].file, (x_pos*tile_size, y_pos*tile_size))
