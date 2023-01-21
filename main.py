72bda344e8d29366aa10499dc0729db2a2353c73
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2023-01-22 00:53:26 +0100

    Implemented wavefunction logic again

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from PIL import Image
 #
 
 class Tile:
-    def __init__(self, coords: tuple[int, int], possible_states: list,valid_neighbours: dict, grid_size: int, ttype: str = "", collapsed = False):
+    def __init__(self, coords: tuple[int, int], possible_states: list, grid_size: int, ttype: str = "", collapsed = False):
 
         self.ttype: str = ttype
         self.file: Image = None
         self.coords: tuple[int, int] = coords
         self.collapsed: bool = collapsed
-        self.valid_neighbours: dict = valid_neighbours
+        self.valid_neighbours: dict = None
         self.possible_states: list = possible_states
         self.grid_size: int = grid_size
         self.north: tuple[int, int] = (max(self.coords[0] - 1, 0), self.coords[1])
@@ -37,16 +37,21 @@ class Tile:
         self.tile_south = _grid[self.south[1]][self.south[0]]
         self.tile_west = _grid[self.west[1]][self.west[0]]
         self.tiles = [self.tile_north, self.tile_east, self.tile_south, self.tile_west]
+
+    def get_states_count(self) -> int:
+        return len(self.possible_states)
         
 
     def update(self, _grid):
         if not self.collapsed:
             return
 
+        directions: list[str] = ["north", "east", "south", "west"]
+
         # Update the state list of all surrounding tiles
-        for tile in self.tiles:
+        for tile, direction in zip(self.tiles, directions):
             if not tile.collapsed:
-                tile.possible_states = list(set(self.valid_neighbours["options"][self.ttype]).intersection(tile.possible_states))
+                tile.possible_states = list(set(self.valid_neighbours[direction]).intersection(tile.possible_states))
 
 def main():
     file = "default"
@@ -85,18 +90,18 @@ def main():
     # Grid size
     size: int = data["meta"]["size"]
 
-    states: list = data["types"]
+    states: list[str] = data["types"]
 
     generate_image(size, data, states, file)
 
 
-def generate_image(size: int, valid_neighbours: dict, possible_states: list, _file: str):
+def generate_image(size: int, data: dict, possible_states: list[str], _file: str):
     # Define and generate initial grid
     grid: list = []
     for y_pos in range(size):
         row = []
         for x_pos in range(size):
-            _tile = Tile((x_pos, y_pos), possible_states, valid_neighbours, size)
+            _tile = Tile((x_pos, y_pos), possible_states, size)
             row.append(_tile)
         grid.append(row)
 
@@ -110,17 +115,17 @@ def generate_image(size: int, valid_neighbours: dict, possible_states: list, _fi
     y_pos = random.randint(0, size - 1)
 
     # Initialize first tile; EVERYTHIN BELOW NEEDS REWORK
-    options = valid_neighbours["types"]
-    index = random.randint(0, len(options) - 1)
-    
-    ttype = options[index]
+    ttype: str = random.choice(possible_states)
 
-    grid[y_pos][x_pos].collapsed = True
-    grid[y_pos][x_pos].ttype = ttype
-    grid[y_pos][x_pos].neighbours = valid_neighbours["options"][ttype]
-    grid[y_pos][x_pos].file = valid_neighbours["image_files"][valid_neighbours["types"].index(ttype)]
+    initial_tile: Tile = grid[y_pos][x_pos]
 
-    tile_size: int = valid_neighbours["meta"]["tile_size"]
+    initial_tile.collapsed: bool = True
+    initial_tile.ttype: str = ttype
+    initial_tile.valid_neighbours: dict = data["options"][ttype]
+    initial_tile.possible_states: list[str] = [ttype]
+    initial_tile.file: Image = data["image_files"][data["types"].index(ttype)]
+
+    tile_size: int = data["meta"]["tile_size"]
 
     
     # Generate clean canvas to later add tiles
@@ -130,6 +135,8 @@ def generate_image(size: int, valid_neighbours: dict, possible_states: list, _fi
     total_start = time.perf_counter()
     while True:
         # Iterate over the whole grid, update all tiles and check if finished; SHOULD WORK FINE
+        min_states = len(possible_states)
+        min_tiles = []
         start = time.perf_counter()
         finished = True
         for y_pos in range(size):
@@ -137,6 +144,30 @@ def generate_image(size: int, valid_neighbours: dict, possible_states: list, _fi
                 grid[y_pos][x_pos].update(grid)
                 if not grid[y_pos][x_pos].collapsed:
                     finished = False
+
+        for y_pos in range(size):
+            for x_pos in range(size):
+                __tile: Tile = grid[y_pos][x_pos]
+                states: int = __tile.get_states_count()
+                if states < min_states:
+                    min_states = states
+                    min_tiles = []
+                    min_tiles.append(__tile)
+                elif states == min_states:
+                    min_tiles.append(__tile)
+
+        lowest_entropy_tile: Tile = random.choice(min_tiles)
+
+        _ttype: str = random.choice(possible_states)
+
+        lowest_entropy_tile.collapsed: bool = True
+        lowest_entropy_tile.ttype: str = _ttype
+        lowest_entropy_tile.valid_neighbours: dict = data["options"][_ttype]
+        lowest_entropy_tile.possible_states: list[str] = [_ttype]
+        lowest_entropy_tile.file: Image = data["image_files"][data["types"].index(_ttype)]
+
+
+
         duration = time.perf_counter() - total_start
 
         # Display time of iteration
